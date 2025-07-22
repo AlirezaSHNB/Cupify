73ca99a61263010a47c7f81c893fb65aa8dad8b1
Alireza Sharifpour <alireza.sh@arianalabs.io>
2025-07-22 15:55:19 +0330

    heyyyy

--- app/models/league.rb
+++ app/models/league.rb
@@ -4,7 +4,7 @@ class League < ApplicationRecord
 
     has_many :league_participant, dependent: :destroy
     has_many :participants, through: :league_participant
-    has_many :matches, dependent: :destroy 
+    has_many :matches, dependent: :destroy
     belongs_to :winner, class_name: "Participant", optional: true
     belongs_to :cup
 
